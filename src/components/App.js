2dcba09e0c583e3f18baca66be3e22d071df164b
Adrenine <adrenine13@gmail.com>
2021-02-15 01:58:14 +0530

    unwanted pictures removed

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
-import Header from './Header';
 import Footer from './Footer';
 import Content from './Content';
-import {device} from '../helper/media';
 
 const Body = styled.div`
   position:relative;
